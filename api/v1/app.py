210c145746fa2ad58ab3321cc20f3c9861179b63
GabrielAbdul <gabrielabdul1@gmail.com>
2020-09-06 21:31:22 +0000

    added custom 404 response, task 6

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 '''app that starts a flask application'''
 
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 
@@ -9,11 +9,19 @@ from api.v1.views import app_views
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix='/api/v1')
 
+
 @app.teardown_appcontext
-def teardown_method(self):
+def app_teardown(self):
     '''teardown_method'''
     storage.close()
 
 
+@app.errorhandler(404)
+def app_errorhandle(self):
+    '''returns json formmated 404 status'''
+    error = {'error': 'Not found'}
+    return jsonify(error), 404
+
+
 if __name__ == '__main__':
-    app.run(threaded=True)
+    app.run(threaded=True, debug=True)
