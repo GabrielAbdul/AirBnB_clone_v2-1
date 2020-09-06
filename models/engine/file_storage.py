287fc85e5bf6c730306380ade56c0ca37d0a5c84
Jacob Ide <jacobmeide@gmail.com>
2020-09-06 17:33:46 +0000

    Added definitions to file storage. One returns an inividual member of a class with provided id. The other returns count of all objects, or all of objects of a type, if class name provided.

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,18 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """Gets all instances of city,state"""
+        if cls is None or id is None:
+            return None
+        for obj in self.all(cls):
+            if id in obj:
+                return self.all(cls).get(obj)
+        return None
+
+    def count(self, cls=None):
+        """Counts all instances"""
+        if cls is None:
+            return len([i for i in self.all()])
+        return len([i for i in self.all(cls)])
