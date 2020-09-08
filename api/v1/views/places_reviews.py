8db4a93c675c723fb9c41022ee06110e0958dac8
GabrielAbdul <gabrielabdul1@gmail.com>
2020-09-08 17:40:31 +0000

    corrected shebang

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -1,21 +1,29 @@
 #!/usr/bin/python3
-"""This module generates views for the User class"""
+"""This module generates views for the State class"""
 
 from api.v1.views import app_views
 from flask import jsonify, request, abort
 from models import storage
-from models.user import User
+from models.review import Review
 
 
-@app_views.route('/users/', strict_slashes=False, methods=['GET', 'POST'])
-def users_no_id():
+@app_views.route('/cities/<place_id>/reviews', strict_slashes=False,
+                 methods=['GET', 'POST'])
+def reviews_no_id(place_id=None):
     '''returns json object and either gets or posts'''
+    # Call state object
+    place_obj = storage.get('Place', place_id)
+
+    # NULL check
+    if place_obj is None:
+        abort(404)
 
     # Read
     if request.method == "GET":
-        users = storage.all('User')
-        list_of_users = [user.to_dict() for user in users.values()]
-        return(jsonify(list_of_users))
+        reviews = storage.all('Review')
+        list_of_reviews = [c_obj.to_dict() for c_obj in reviews.values()
+                          if c_obj.place_id == place_id]
+        return(jsonify(list_of_reviews))
 
     # Create
     if request.method == "POST":
@@ -27,18 +35,16 @@ def users_no_id():
         # no name check
         if j_obj.get('name') is None:
             abort(400, 'Missing name')
-        if j_obj.get('email') is None:
-            abort(400, 'Missing email')
-        if j_obj.get('password') is None:
-            abort(400, 'Missing password')
-        new_user = User(**j_obj)
-        new_user.save()
-        return(jsonify(new_object.to_dict()), 201)
+        j_obj['place_id'] = place_id
+        new_review = Review(**j_obj)
+        new_review.save()
+        return(jsonify(new_review.to_dict()), 201)
 
 
-@app_views.route('/users/<id>', strict_slashes=False, methods=["GET","PUT", "DELETE"])
-def users_with_id(id):
-    obj = storage.get('User', id)
+@app_views.route('/reviews/<id>',
+                 strict_slashes=False, methods=["GET","PUT", "DELETE"])
+def reviews_with_id(id):
+    obj = storage.get('Review', id)
     if obj is None:
         abort(404)
     # Read
@@ -58,7 +64,7 @@ def users_with_id(id):
         if j_obj is None:
             abort(400, 'Not a JSON')
         # List of objects to be ignored on iteration
-        ignore_list = ['id','created_at','updated_at']
+        ignore_list = ['id','created_at','updated_at','place_id']
         # create dictionary of items to be updated
         bounce = {k: v for k, v in j_obj.items() if k not in ignore_list}
         # iter. across dict of items to be updated; setattr to update class obj
